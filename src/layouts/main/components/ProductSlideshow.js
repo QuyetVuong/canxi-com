031415ce232194b4540b710fd953de15f4b099c4
VuongQuyet <Vuong.Quyet0@gmail.com>
2024-08-12 02:49:58 +0700

    update

--- src/layouts/main/components/ProductSlideshow.js
+++ src/layouts/main/components/ProductSlideshow.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Slider from 'react-slick';
-import product1 from '../../../assets/images/show1.jpg';
 import product from '../../../assets/images/show.jpg';
+import product1 from '../../../assets/images/show1.jpg';
 import product2 from '../../../assets/images/show2.jpg';
 import product3 from '../../../assets/images/show3.jpg';
+import product4 from '../../../assets/images/show4.jpg';
+import product5 from '../../../assets/images/show5.jpg';
 
 export function ProductSlideshow() {
     const settings = {
@@ -32,6 +34,12 @@ export function ProductSlideshow() {
             </div>
             <div>
               <img src={product3} alt="Product 4" />
+            </div>
+            <div>
+              <img src={product4} alt="Product 5" />
+            </div>
+            <div>
+              <img src={product5} alt="Product 6" />
             </div>
           </Slider>
         </div>
